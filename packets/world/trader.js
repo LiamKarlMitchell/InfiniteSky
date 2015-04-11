bd026abf5302ecabc31692e8f4811c6fac9b61e2
Carlitoz <c.alberto_94@hotmail.com>
2015-04-11 02:27:08 -0400

    Figured out packet for trader items
    Issue #160
    Items list packet implemented for Fujin (Should work on all
    factions just have to get the right Key value)

--- packets/world/trader.js
+++ packets/world/trader.js
@@ -2,4 +2,49 @@
 // Copyright (c) InfiniteSky Dev Teams - Licensed under GNU GPL
 // For more information, see LICENCE in the main folder
 
-//89 Withdraw
+WorldPC.NPCFunction = restruct.
+int32lu('Key').
+int32lu('Value');
+
+WorldPC.TraderItem = restruct.
+int32lu('Slot').
+string('Username', 14).
+int16lu('Unknown').
+int32lu('ItemID').
+int32lu('Amount').
+int8lu('Enchant').
+int8lu('Combine').
+int16lu('Growth').
+int32lu('Price');
+
+WorldPC.TraderItems = restruct.struct('Items', WorldPC.TraderItem, 400);
+
+WorldPC.Set(0x59, {
+    Restruct: WorldPC.NPCFunction,
+
+    function: function NPCFunctionRecv(client, input) {
+        console.log("NPCFunction packet received: ", input);
+	   	// Open Trader Items
+		if (input.Key === 6 && input.Value === 1) {
+		  // Send trader items to client.
+		  client.write(
+		  	packets.makeCompressedPacket( 0x77, 
+		  	  new Buffer(
+		  	  	WorldPC.TraderItems.pack(
+		  	  		{ Items: [ 
+		  	  			{ 
+		  	  				Slot: 1,
+		  	  				Username: 'Admin',
+		  	  				ItemID: 1,
+		  	  				Amount: 1337,
+		  	  				Price: 1
+		  	  			}]
+		  	       }
+		  	    )
+		  	  )
+		  	)
+		  );
+		} else {
+		  client.sendInfoMessage('Unhandled NPCFunction Key '+input.Key+' '+input.Value);
+		}}
+});
