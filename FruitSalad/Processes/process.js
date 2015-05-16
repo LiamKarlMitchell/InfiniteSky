8170d2a3c12e51b63bd7e50731e44a3d11716d38
Liam Mitchell <liamkarlmitchell@gmail.com>
2015-05-17 03:06:21 +1200

    Fixed conflict in process.js

--- FruitSalad/Processes/process.js
+++ FruitSalad/Processes/process.js
@@ -1,44 +1,13 @@
 // setup enviorment for child process
+fs = require('fs');
+// vm = require('vm');
+// path = require('path');
 
-// looks at arguments, first is script name to run as entrypoint.
-// other arguments could be passed to its constructor.
-// 'Login'
-// Login/Login.js
-//  etc...
+vmscript = new (require('../vmscript.js'))();
+vms = global.vmscript.vms;
+api = {};
 
-// vms.loadFile(entrypoint);
+api.spawnScript = vmscript.watch;
 
-// global api;
-var fs = require('fs');
-var vm = require('vm');
-var path = require('path');
-
-global.api = {};
-
-global.api.runInContext = function(file_path){
-	// vms.loadFIle(entrypoint) instead
-	// client.agent.api.test = function(){
-	// 	console.log("Altered api function");
-	// }
-
-	// global.spawner_api.validateAPI(process.pid);
-
-	fs.readFile(file_path, function(err, content){
-		if(err){
-
-			console.log(err);
-			return;
-		}
-
-
-		var code = content.toString();
-		global.vms = function(name, depends, callback){
-
-		}
-		global.require = require;
-		vm.runInThisContext(code);
-	});
-}
-
-var ChildSpawner = require('../Helper/ChildSpawner.js');
+ChildSpawner = require('../Helper/ChildSpawner.js');
 var client = ChildSpawner.Resume();
