73c9b9919cce25eb0f43eddbead833ded7088267
Liam Mitchell <liamkarlmitchell@gmail.com>
2015-05-21 01:17:02 +1200

    Updated itemsToMongo.js

--- FruitSalad/Grunt/itemsToMongo.js
+++ FruitSalad/Grunt/itemsToMongo.js
@@ -1,25 +1,27 @@
 //vmscript.watch('Config/login.json');
 var vmscript = new (require('../vmscript.js'))();
-//config = {};
 Database = require('../Modules/db.js');
-vmscript.watch('../Config/world.json');
 
 module.exports = function(grunt) {
   grunt.registerTask('itemsToMongo', 'Loads items from the game file 005_00002.IMG into Mongo.', function() {
   	var done = this.async();
 
-  	if (!config.database || !config.database.connection_string) {
-  		throw new Error('Expecting config.database.connection_string to be set.');
-  	}
+  	vmscript.on(['config'], function() {
 
-  	if (!config.info_directory) {
-  		throw new Error('Expecting config.info_directory to be set. Please run grunt init or grunt locateGameFiles.');
-  	}
+	  	if (!config.database || !config.database.connection_string) {
+	  		throw new Error('Expecting config.database.connection_string to be set.');
+	  	}
 
-	Database(config.database.connection_string, function(){
-		console.log("Database connected");
-		vmscript.watch('Database/item.js');
-	});
+	  	if (!config.info_directory) {
+	  		throw new Error('Expecting config.info_directory to be set. Please run grunt init or grunt locateGameFiles.');
+	  	}
+
+		Database(config.database.connection_string, function(){
+			console.log("Database connected");
+			vmscript.watch('Database/item.js');
+		});
+
+  	});
 
   	vmscript.on(['items'], function() {
   		// TODO: Load items from Game File
@@ -29,6 +31,8 @@ module.exports = function(grunt) {
   	});
   	//database
 	//connection_string
+
+	vmscript.watch('Config/world.json');
   });
 };
 
