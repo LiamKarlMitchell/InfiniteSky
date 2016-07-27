4f1af2237670950bfcb4acf0aa2c8e7b51300829
Arafy <inplacavel2007@hotmail.com>
2016-07-27 17:37:18 +0200

    gm command to clear inventory

---
+++ FruitSalad/Processes/Zone/Commands/clearinventory.js
@@ -0,0 +1,16 @@
+// This file is part of InfiniteSky.
+// Copyright (c) InfiniteSky Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+/////////////////////////////////////////////////////////////
+// Command: clearinventory
+// clears the characters inventory
+GMCommands.AddCommand(new Command('clear',20,function command_clearinventory(string,client){
+		// Clear the inventory
+		for (var i=0;i<client.character.Inventory.length;i++) {
+			client.character.Inventory[i] = null;
+		}
+		client.character.markModified('Inventory');
+		client.character.save();
+        client.sendInfoMessage('Please log out and back in for changes to apply or type /load.');
+}));
